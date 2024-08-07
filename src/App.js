35adf87f6496c99222ce2a9533eb7c64e247fb44
Anuj Rawat <anujrawat9639@gmail.com>
2024-08-07 15:23:32 +0530

    searchgpt done

--- src/App.js
+++ src/App.js
@@ -18,16 +18,14 @@ function App() {
         
         <div className='built'>
           <div className='super'>
-            <p>Built by</p>
-            <img src={logo} alt="Supermemory Logo" />
-            <a href="https://supermemory.ai">Supermemory.ai</a>
+            <p>Built by : DonoBhai.ai</p>
+           
           </div>
 
           <div className='memo'>
-            <p>Personalization by</p>
-            <a href="https://memo.com" target="_blank" rel="noopener noreferrer">
-              <img src={memo} alt="Memo" style={{ cursor: 'pointer' }} />
-            </a>
+            <p>Personalization by : WALIA</p>
+           
+           
           </div>
         </div>
       </div>
